b15c3ce26b32ecca3bcb5a0ada66d9b489459d3a
AudeScript <sopbweaudrey@gmail.com>
2025-11-25 09:48:53 +0100

    initial commit with navigation and sidebar layout

---
+++ constants/index.ts
@@ -0,0 +1,12 @@
+const sideBarLinks = [
+  { imageUrl: "/icons/Home.svg", label: "Home", route: "/" },
+  { imageUrl: "/icons/upcoming.svg", label: "Upcoming", route: "/upcoming" },
+  { imageUrl: "/icons/previous.svg", label: "Previous", route: "/previous" },
+  { imageUrl: "/icons/Video.svg", label: "Recordings", route: "/recordings" },
+  {
+    imageUrl: "/icons/add-personal.svg",
+    label: "Personal Room",
+    route: "/personal-room",
+  },
+];
+export default sideBarLinks;
